86a62054681c958a21e88a9bc5a64cd468f19de2
HseinKt <hseinkteish1998@gmail.com>
2023-03-10 16:54:08 +0200

    finished login page

--- connection_db.php
+++ connection_db.php
@@ -4,12 +4,12 @@ $database = 'healthnet_db';
 $username = 'root';
 $password = '';
 
-$link = new mysqli($host_name, $username, $password, $database);
+$mysqli = new mysqli($host_name, $username, $password, $database);
 
 header("Access-Control-Allow-Origin: * ");
 header("Access-Control-Allow-Headers:*");
-if ($link->connect_error) {
-    die('<p>Failed to connect to MySQL: ' . $link->connect_error . '</p>');
+if ($mysqli->connect_error) {
+    die('<p>Failed to connect to MySQL: ' . $mysqli->connect_error . '</p>');
 }
 
 
