0c92cca7db52618d3d102faca1e45559a8e359d8
HseinKt <hseinkteish1998@gmail.com>
2023-03-10 18:11:06 +0200

    finished register page

--- login_api.php
+++ login_api.php
@@ -1,35 +1,34 @@
 <?php
 include "connection_db.php";
 
-   
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $usertype_id = $_POST['usertype_id'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$usertype_id = $_POST['usertype_id'];
 
-        $query = $mysqli -> prepare("select * from users where email = ?");
-        $query -> bind_param('s',$email);
-        $query -> execute();
-        $query -> store_result();
-        $num_rows = $query -> num_rows();
-        $query -> bind_result($id, $name, $email, $hashed_password, $dob, $usertype_id );
-        $query -> fetch();
-        $response = [];
-        
-        if ($num_rows == 0) {
-            $response['response'] = "user not found";
-        }
-        else {
-            if (password_verify($password,$hashed_password)) {
-                $response['response'] = "logged in";
-                $response['name'] = $name;
-                $response['email'] = $email;
-                $response['usertype_id'] = $usertype_id;
-            }
-            else {
-                $response['response'] = "Incorrect password";
-            }
-        }
-        echo json_encode($response);
+$query = $mysqli -> prepare("select * from users where email = ?");
+$query -> bind_param('s',$email);
+$query -> execute();
+$query -> store_result();
+$num_rows = $query -> num_rows();
+$query -> bind_result($id, $name, $email, $hashed_password, $dob, $usertype_id );
+$query -> fetch();
+$response = [];
+
+if ($num_rows == 0) {
+    $response['response'] = "user not found";
+}
+else {
+    if (password_verify($password,$hashed_password)) {
+        $response['response'] = "logged in";
+        $response['name'] = $name;
+        $response['email'] = $email;
+        $response['usertype_id'] = $usertype_id;        
+    }
+    else {
+        $response['response'] = "Incorrect password";
+    }
+}
+echo json_encode($response);
 
 ?>
 
---
+++ register_api.php
@@ -0,0 +1,30 @@
+<?php
+
+include "connection_db.php";
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$dob = $_POST['dob'];
+$usertype_id = 1;
+
+$check_email = $mysqli->prepare('select email from users where email=?');
+$check_email->bind_param('s', $email);
+$check_email->execute();
+$check_email->store_result();
+$num_rows = $check_email->num_rows();
+
+$hashed_password = password_hash($password, PASSWORD_BCRYPT);
+$response = [];
+
+if ($num_rows > 0) {
+    $response['status'] = "failed";
+} else {
+    $query = $mysqli->prepare('insert into users(name,email,password,dob,usertype_id ) values(?,?,?,?,?)');
+    $query->bind_param('ssssi', $name, $email, $hashed_password, $dob, $usertype_id );
+    $query->execute();
+    $response['status'] = "success";
+}
+
+echo json_encode($response);
+
