ad3b6acc662c2d2e30547f5ec37e9e3b2bc7ba1b
HseinKt <hseinkteish1998@gmail.com>
2023-03-11 16:25:12 +0200

    finished the display medications page

--- display_medications.php
+++ display_medications.php
@@ -18,27 +18,3 @@ while ($row = $display_medication -> fetch_array(MYSQLI_NUM)) {
 echo json_encode($response);
 
 ?>
-
-
-
-<!-- 
-
-$user_id = $_GET['user_id'];
-$medication_id = $_GET['medication_id'];
-
-'SELECT *
-FROM medications m
-JOIN users_medications um ON m.id = um.medication_id 
-JOIN users u ON u.id = um.user_id
-WHERE u.id = ?'); 
-
-$display_medication->bind_param('i', $user_id);
-$display_medication->execute();
-$display_medication->store_result();
-$num_rows = $display_medication->num_rows();
-
-$query = $mysqli->prepare('insert into users(name,email,password,dob,usertype_id ) values(?,?,?,?,?)');
-    $query->bind_param('ssssi', $name, $email, $hashed_password, $dob, $usertype_id );
-    $query->execute();
-    $response['status'] = "success";
--->
